4963eb4859397190c12fca64201ff29e6a7d0daf
sysn00ps <sysn00ps@users.noreply.github.com>
2014-09-30 11:08:17 +0200

    Adding the main method

--- FirstFile.java
+++ FirstFile.java
@@ -1,9 +1,14 @@
 public class FirstFile {
 
-// Declaration of variable
-String message = "Hello GitHub!"
+  // Declaration of variable
+  String message = "Hello GitHub!"
 
-// Print message on the screen
-System.out.println(message);
+  // The main method
+  public static void main (String[] args) {
+  
+    // Print message on the screen
+  System.out.println(message);
 
-}
+  } // end of main
+
+} // end of class
